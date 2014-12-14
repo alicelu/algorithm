bf23cc55676fc7b75a870940413f02162833d890
alicelu <lusha.apple@gmail.com>
2014-12-14 13:05:57 +0800

    Add MergeIntervals

--- algorithm/src/com/uniquesoft/leetcode/MergeIntervals.java
+++ algorithm/src/com/uniquesoft/leetcode/MergeIntervals.java
@@ -4,6 +4,8 @@
 package com.uniquesoft.leetcode;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 /**
@@ -12,9 +14,14 @@ import java.util.List;
  */
 public class MergeIntervals {
 
-    public class Interval {
+    public static class Interval {
         int start;
         int end;
+
+        public Interval() {
+            start = 0;
+            end = 0;
+        }
 
         public Interval(int s, int e) {
             start = s;
@@ -25,6 +32,30 @@ public class MergeIntervals {
     public static List<Interval> mergeIntervals(List<Interval> intervals) {
         List<Interval> result = new ArrayList<Interval>();
 
+        Collections.sort(intervals, new IntervalComparator());
+
+        Interval lastInterval = intervals.get(0);
+        result.add(lastInterval);
+
+        for (int i = 1; i < intervals.size(); i++) {
+            Interval itv = intervals.get(i);
+            if (itv.start > lastInterval.end) {
+                lastInterval = itv;
+                result.add(lastInterval);
+            } else {
+                lastInterval.end = lastInterval.end > itv.end ? lastInterval.end : itv.end;
+            }
+        }
+
         return result;
+    }
+
+    private static class IntervalComparator implements Comparator<Interval> {
+
+        @Override
+        public int compare(Interval o1, Interval o2) {
+            return o1.start - o2.start;
+        }
+
     }
 }
--- algorithm/test/com/uniquesoft/leetcode/TestMergeIntervals.java
+++ algorithm/test/com/uniquesoft/leetcode/TestMergeIntervals.java
@@ -3,6 +3,8 @@
  */
 package com.uniquesoft.leetcode;
 
+import static org.junit.Assert.assertEquals;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,14 +22,22 @@ public class TestMergeIntervals {
     public void testMergeIntervals() {
         List<Interval> intervals = new ArrayList<Interval>();
 
-        MergeIntervals mi = new MergeIntervals();
-        intervals.add(mi.new Interval(1, 3));
-        intervals.add(mi.new Interval(2, 6));
-        intervals.add(mi.new Interval(8, 10));
-        intervals.add(mi.new Interval(15, 18));
+        intervals.add(new Interval(1, 3));
+        intervals.add(new Interval(2, 6));
+        intervals.add(new Interval(8, 10));
+        intervals.add(new Interval(10, 14));
+        intervals.add(new Interval(15, 19));
+        intervals.add(new Interval(16, 17));
+        intervals.add(new Interval(19, 21));
 
         List<Interval> result = MergeIntervals.mergeIntervals(intervals);
 
+        assertEquals(1, result.get(0).start);
+        assertEquals(6, result.get(0).end);
+        assertEquals(8, result.get(1).start);
+        assertEquals(14, result.get(1).end);
+        assertEquals(15, result.get(2).start);
+        assertEquals(21, result.get(2).end);
     }
 
 }
